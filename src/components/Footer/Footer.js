7b1ea5f131a8707a92ab5203a994173166215acb
Marius <Marius.Supercode@gmail.com>
2022-12-11 18:23:51 +0100

    fixed errors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,28 +2,28 @@
 
 function Footer() {
     return (
-        <footer class="footer">
-            <p class="p1">The Design Shop</p>
-            <p class="p2">Contact</p>
-            <p class="p3">Newsletter</p>
-            <p class="p4">Find Us Here</p>
-            <div class="div1">
+        <footer className="footer">
+            <p className="p1">The Design Shop</p>
+            <p className="p2">Contact</p>
+            <p className="p3">Newsletter</p>
+            <p className="p4">Find Us Here</p>
+            <div className="div1">
                 <p>Crafeully curated online design store,<br></br> founded and designd by <a href="https">Ruby Bacanovic.</a></p>
             </div>
-            <div class="div2">
+            <div className="div2">
                 <a href="https">Email us</a>
                 <a href="https">Work with us</a>
             </div>
-            <div class="div3">
+            <div className="div3">
                 <p>Click here to <a href="https">subscribe.</a></p>
                 <p >Don't worry. We hate spam too</p>
             </div>
-            <div class="div4">
+            <div className="div4">
                 <a href="https">facebook</a>
                 <a href="https">Instagram</a>
                 <a href="https">Twitter</a>
             </div>
-            <p class="cc">©2019 The Design Shop • The Design Blog</p>
+            <p className="cc">©2019 The Design Shop • The Design Blog</p>
         </footer>
     );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,9 @@ import Data from '../../Product Data.json';
 
 function importAll(r) {
     let images = {};
-    r.keys().map(item => { images[item.replace('./', '')] = r(item); });
+    r.keys().forEach(item => { images[item.replace('./', '')] = r(item); });
     return images;
 }
-// console.log(require.context('../../img', false, /.(png|jpe?g|svg)$/));
 
 const images = importAll(require.context('../../img', false, /.(png|jpe?g|svg)$/));
 
@@ -15,10 +14,8 @@ function Main() {
     return (
         <main>
             {Data.map((dataElement, index) => {
-                {/* console.log(images[dataElement.ProductPicture]); */ }
-                {/* import img from `../../img/${dataElement.Product - picture}`; */ }
                 return (
-                    <figure class={`fig${index + 1}`}>
+                    <figure key={index} className={`fig${index + 1}`}>
                         <img src={images[dataElement.ProductPicture]} alt={dataElement.ProductName}></img>
                         <figcaption>
                             <h4>{dataElement.ProductName}<span>{dataElement.Price}</span></h4>
